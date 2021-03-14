8a09077e947bbe08bb42c2cbead8eeebbb6d3ad5
Buzinaev Temir <heavy421@gmail.com>
2021-03-14 19:04:54 +0900

    Initial commit

---
+++ app/src/main/java/com/example/jsontest_buzinaev/JsontestAPI.java
@@ -0,0 +1,11 @@
+package com.example.jsontest_buzinaev;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsontestAPI {
+    // Образец
+    // http://date.jsontest.com/
+    @GET("/")
+    public Call<ServerTime> getServerDateTime();
+}
